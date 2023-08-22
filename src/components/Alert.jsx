82d90fe7e697f593a188a9b2d32cbd1f05365c42
Raziel1212 <104324252+Raziel1212@users.noreply.github.com>
2023-08-22 00:43:20 -0400

    Se agrega componente Alert

---
+++ src/components/Alert.jsx
@@ -0,0 +1,14 @@
+
+const Alert = ({msg, color}) =>{
+
+    return(
+        <>
+        <div className={`alert alert-${color} my-2`}>
+        <p>{msg}</p>   
+        </div>
+        </>
+
+    )
+}
+
+export default Alert
