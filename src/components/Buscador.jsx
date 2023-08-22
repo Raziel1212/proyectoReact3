073c4a50771529249ce1cff93c2e30e376dd8047
Raziel1212 <104324252+Raziel1212@users.noreply.github.com>
2023-08-22 00:44:28 -0400

    se agrega componente Buscador

---
+++ src/components/Buscador.jsx
@@ -0,0 +1,20 @@
+
+
+const Buscador = ({onChange, search }) => {
+
+    return(
+
+        <form className="d-flex mb-3" role="search">
+        <input 
+        className="form-control me-2" 
+        type="search" 
+        placeholder="Busca un colaborador" 
+        aria-label="Search"
+        value={search}
+        onChange={onChange}
+        />
+      </form>
+    )
+}
+
+export default Buscador
